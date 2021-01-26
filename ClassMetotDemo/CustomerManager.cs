fcfb0bbfcfd996a5a610d67a029643180af4e05e
EMN <EMN@DESKTOP-E912P98>
2021-01-26 23:17:09 +0300

    Yazılım Kampı 3.hafta ödevi

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.MusteriNo +" nolu "+ customer.Name +" "+ customer.Surname +" eklendi.");
+        }
+
+        public void Info(Customer customer)
+        {
+            Console.WriteLine("Müşteri ID : "+ customer.Id);
+            Console.WriteLine("Müşteri İsim : " + customer.Name);
+            Console.WriteLine("Müşteri Soyisim : " + customer.Surname);
+
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 0000001;
+            customer1.MusteriNo = "01";
+            customer1.Name = "Emin";
+            customer1.Surname = "Aydın";
+            customer1.TcNo = "12345678900";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 0000002;
+            customer2.MusteriNo = "02";
+            customer2.Name = "Anushka";
+            customer2.Surname = "Sharma";
+            customer2.TcNo = "12345678901";
+
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Info(customer2);
+        }
+
+        
+        
+        
+        
+           
+
+
+
+        
+    }
+}
